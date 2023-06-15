e2bd2f3241c825b4d12de3f5220e9a1967365b9e
kevin <kevin@users.noreply.github.com>
2023-06-15 14:00:51 -0500

    basic imgui setup

---
+++ terminal/src/main/java/terminal/Main.java
@@ -0,0 +1,21 @@
+package terminal;
+
+import imgui.ImGui;
+import imgui.app.Application;
+import imgui.app.Configuration;
+
+public class Main extends Application {
+    @Override
+    protected void configure(Configuration config) {
+        config.setTitle("COMET");
+    }
+
+    @Override
+    public void process() {
+        ImGui.text("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        launch(new Main());
+    }
+}
